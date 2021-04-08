d360deefb5ec116c430f20468f55c64fd8f4bda2
Gabriel Rezende <g@brielrezen.de>
2021-04-08 20:38:25 -0300

    pasta src criada

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 
 app.listen(3000, () => {
   console.log('server rodando na porta 3000')
 })
+
+app.use(cors())
