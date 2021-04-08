a2c4f825a31ca8a5def7375f86875c1d5defaf28
Gabriel Rezende <g@brielrezen.de>
2021-04-08 20:46:27 -0300

    porta 3333 e contenttype para json

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 
-app.listen(3000, () => {
-  console.log('server rodando na porta 3000')
+
+app.listen(3333, () => {
+  console.log('server rodando na porta 3333')
 })
 
 app.use(cors())
+app.use(express.json()) // Content-type
