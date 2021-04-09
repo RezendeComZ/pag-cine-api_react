e1517343d7c7620ff7f06c9e93a1acb38fd5168f
Gabriel Rezende <g@brielrezen.de>
2021-04-09 11:48:12 -0300

    migration de filmes, não usei, não tenho certeza se funciona ainda

--- src/database/migrations/20210409021815-create_filmes_table.js
+++ src/database/migrations/20210409021815-create_filmes_table.js
@@ -2,12 +2,46 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('filmes', {
+      id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+      },
+      titulo: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      tituloOriginal: {
+        type: Sequelize.STRING,
+      },
+      lancamento: Sequelize.DATEONLY,
+      paisOrigem: Sequelize.STRING,
+      duracao: Sequelize.TIME,
+      sinopse: Sequelize.TEXT,
+      rate: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+      },
+      exibicaoInicio: Sequelize.DATEONLY,
+      exibicaoFinal: Sequelize.DATEONLY,
+      idioma_id: {
+        type: Sequelize.INTEGER,      
+      },
+      genero_id: Sequelize.INTEGER,
+      poster_url: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      trailer_url: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      classificacao: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+      }
+    })
   },
 
   down: async (queryInterface, Sequelize) => {
