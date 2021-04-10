a9d7b6f876446cdec6eb1637582a971eb6472fdc
Gabriel Rezende <g@brielrezen.de>
2021-04-10 10:11:24 -0300

    rota de retorno de json ok

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -7,5 +7,9 @@ module.exports = {
   async listaFilmes(req, res, next) {
     const lista = await Filme.findAll();
     res.send(lista)
+  },
+  retornoJson(req, res) {
+    let textos = req.body
+    res.json(textos)
   }
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,6 @@ const router = Router()
 
 router.get('/', indexController.index)
 router.get('/listaFilmes', indexController.listaFilmes)
+router.post('/retornoJson', indexController.retornoJson)
 
 module.exports = router
