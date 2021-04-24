6537467699d3ba9df0a78dfc4c1eaee6e0eb3aae
Gabriel Rezende <g@brielrezen.de>
2021-04-24 18:27:30 -0300

    endpoint de ID única de filme

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -8,6 +8,16 @@ module.exports = {
     const lista = await Filme.findAll();
     res.send(lista)
   },
+  async filme(req, res, next) {
+    const { id } = req.params
+    const filmeId = await Filme.findOne({
+      where: {
+        id: id
+      }
+    });
+    console.log('entrou aqui')
+    res.send(filmeId)
+  },
   retornoJson(req, res) {
     let textos = req.body
     res.json(textos)
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
-const { Router } = require('express')
+const {
+  Router
+} = require('express')
 const indexController = require('../controllers/indexController')
 const router = Router()
 
 router.get('/', indexController.index)
 router.get('/listaFilmes', indexController.listaFilmes)
+router.get('/filme/:id', indexController.filme)
 router.post('/retornoJson', indexController.retornoJson)
 
 module.exports = router
